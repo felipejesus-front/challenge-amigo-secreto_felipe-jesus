0c581f5ac6266d199d1fa1e3896ce0604b60a84e
Felipe Campos <felipe.campos@al.resilia.com.br>
2025-07-27 17:16:42 -0300

    terminando o que foi pedido no projeto

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+const nomeDoAmigo = document.getElementById("amigo");
+const listaAmigos = document.getElementById("listaAmigos");
+const resultado = document.getElementById("resultado");
+const arrayNomes = [];
+
+function adicionarAmigo() {
+	if (nomeDoAmigo.value.trim()) {
+		arrayNomes.push(nomeDoAmigo.value.trim());
+
+		nomeDoAmigo.value = "";
+
+		preencherLista(arrayNomes);
+	} else {
+		alert("Por favor, insira um nome neste campo.");
+	}
+}
+
+function preencherLista(arrayNomes) {
+	listaAmigos.innerHTML = "";
+
+	arrayNomes.forEach((nome) => {
+		const li = document.createElement("li");
+		li.textContent = nome;
+		listaAmigos.appendChild(li);
+	});
+}
+
+function sortearAmigo() {
+	if (arrayNomes.length > 0) {
+		const gerarNumeroBaseadoNoArray = Math.floor(
+			Math.random() * arrayNomes.length
+		);
+
+		//usar o numero aleatório para escolher um nome da lista
+		console.log(arrayNomes[gerarNumeroBaseadoNoArray]);
+
+		//mostrar resultado na tela
+		resultado.innerHTML = `<p>O amigo secreto sorteado é: <b>${arrayNomes[gerarNumeroBaseadoNoArray]}</b></p>`;
+	} else {
+		alert("A lista está vazia, Coloque pelomenos um nome na lista.");
+	}
+}
